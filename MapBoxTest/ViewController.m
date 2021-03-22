c86a9f51b189b12e2f15d0835d81cbf7e0db27ad
Soran-i <51769397+Soran-i@users.noreply.github.com>
2021-03-22 13:03:52 -0400

    Update ViewController.m

--- MapBoxTest/ViewController.m
+++ MapBoxTest/ViewController.m
@@ -19,6 +19,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     
+    NSURL *url = [NSURL URLWithString:@"mapbox://styles/mapbox/streets-v11"];
     MGLMapView *mapView = [[MGLMapView alloc] initWithFrame:self.view.bounds];
     mapView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
     [mapView setCenterCoordinate:CLLocationCoordinate2DMake(40.74699, -73.98742)
@@ -27,6 +28,19 @@
     [self.view addSubview:mapView];
 
     mapView.styleURL = [MGLStyle satelliteStreetsStyleURL];
+    
+
+    
+    // Add a point annotation
+    MGLPointAnnotation *annotation = [[MGLPointAnnotation alloc] init];
+    annotation.coordinate = CLLocationCoordinate2DMake(40.77014, -73.97480);
+    annotation.title = @"Central Park";
+    annotation.subtitle = @"The best park in New York City!";
+    [mapView addAnnotation:annotation];
+    
+    // Display map location
+    // Allow the map view to display the user's location
+    mapView.showsUserLocation = YES;
     
     
 }
